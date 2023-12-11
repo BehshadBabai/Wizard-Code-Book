86adb6fac010bb4f095901551034c58cec1a2764
BehshadBabai <behshad.babai@gmail.com>
2023-12-10 23:14:41 -0800

    setuo login, change title

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CellList from './components/cellList';
+
+const App: React.FC = () => {
+  return <CellList />;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,14 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CellList from './components/cellList';
+import App from './App';
 
-const App = () => {
-  
+const Index = () => {
   return (
-  <Provider store={store}>
-    <div>
-      <CellList />
-    </div>
-  </Provider>
-  )
-}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
 
-ReactDom.render(<App />,document.querySelector('#root'))
+ReactDom.render(<Index />, document.querySelector('#root'));
